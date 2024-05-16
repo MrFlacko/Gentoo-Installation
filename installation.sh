29722beee54c95259a98509cfe1c411330984413
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2024-05-16 20:27:47 +1000

    Formatting Drive
    Rough type up of formatting the drive

--- installation.sh
+++ installation.sh
@@ -1,2 +1,38 @@
 #!/bin/bash
-echo 'Do stuff here :)'
+
+formatDisk() {
+    ## Main Formatting of the drive, this will create 512MB partition at the start of the drive for EFI
+    ## and create a 4GB partition for the Swap, finally the rest of the drive placed as the linux filesystem
+    
+    # List the drives first so the user can choose them
+    lsblk
+    read -p "Enter the drive you want to format (e.g., /dev/sda): " drive
+    read -p "You have chosen to format $drive. This will erase all data. Are you sure? (yes/no): " confirmation
+    [[ $confirmation != "yes" ]] && echo "Byyee!" && exit 1
+
+    # Unmount the drive if it's mounted
+    umount ${drive}* 2> /dev/null
+
+    # Creating of the partitions
+    clear
+    echo "Creating partitions..."
+    parted -s $drive mklabel gpt \
+        mkpart primary 1MiB 513MiB \
+        mkpart primary 513MiB 4617MiB \
+        mkpart primary 4617MiB 100%
+
+    # Set to EFI
+    parted -s $drive set 1 esp on
+    mkfs.fat -F 32 ${drive}1
+
+    # Create and turn on swap
+    mkswap ${drive}2
+    swapon ${drive}2
+
+    # Format last drive as ext3
+    mkfs.ext4 ${drive}3
+
+    echo "Drive $drive has been formatted. Lets Go!"
+}
+
+formatDisk
